c0c4a64da78b72903598df9cfd622047c4589f87
Abtahi Hasan <abtahihasan7472@gmail.com>
2026-03-15 14:57:06 +0600

    staff login and get profile data added

--- src/redux/apis/authApi.ts
+++ src/redux/apis/authApi.ts
@@ -4,12 +4,19 @@ const authApi = api.injectEndpoints({
   endpoints: (builder) => ({
     staffLogin: builder.mutation({
       query: (payload) => ({
-        url: "",
+        url: "/api/auth/login",
         method: "POST",
+        body: payload,
+      }),
+    }),
+    getMe: builder.query({
+      query: (payload) => ({
+        url: "api/auth/me",
+        method: "GET",
         body: payload,
       }),
     }),
   }),
 });
 
-export const { useStaffLoginMutation } = authApi;
+export const { useStaffLoginMutation, useGetMeQuery } = authApi;
