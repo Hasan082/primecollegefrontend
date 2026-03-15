fac65c4352401de5eb291408a1d04bc7f224a67e
Abtahi Hasan <abtahihasan7472@gmail.com>
2026-03-15 14:41:20 +0600

    config add auto token refresh added

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,9 +1,55 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { appConfig } from "@/app.config";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 
-export const api = createApi({
+const baseQuery = fetchBaseQuery({
+  baseUrl: appConfig.API_BASE_URL,
+  credentials: "include",
+});
+
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_BASE_URL + "/auth/refresh-token",
+      {
+        method: "GET",
+        credentials: "include",
+      },
+    );
+
+    await res.json();
+  }
+
+  return result;
+};
+
+export const base_api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
-  }),
+  baseQuery: baseQueryWithRefreshToken,
+  tagTypes: [
+    "exams",
+    "exam",
+    "subjects",
+    "topics",
+    "auth",
+    "taken-exams",
+    "users",
+    "subscription",
+    "notes",
+    "notice",
+  ],
   endpoints: () => ({}),
 });
