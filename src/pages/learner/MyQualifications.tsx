51533d4a1a9877f592d5f297fceecd928f9e9f88
Md Hasanuzzaman <dr.has82@gmail.com>
2026-03-28 16:52:07 +0600

    Udpate typescript error

--- src/pages/learner/MyQualifications.tsx
+++ src/pages/learner/MyQualifications.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Eye, CalendarPlus, AlertTriangle, Loader2 } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
